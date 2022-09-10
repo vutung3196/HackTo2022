4197252540a43ad5e77e9e76edb71fd0bf70af88
TungVu <tungvu3196@gmail.com>
2022-09-10 15:24:24 -0400

    Update the fetch data

--- frontend/hackto2022/src/components/Map.jsx
+++ frontend/hackto2022/src/components/Map.jsx
@@ -1,16 +1,18 @@
 import { useState, useCallback } from "react";
 import { GoogleMap } from "@react-google-maps/api";
+import DataService from "../services/data.service.js";
 const center = {
   lat: -3.745,
   lng: -38.523,
 };
 const containerStyle = {
   width: "100vw",
-  height: "100hw",
+  height: "100vh",
 };
 
 export default function Map() {
   const [map, setMap] = useState(null);
+  const [location, setLocation] = useState("");
 
   const onLoad = useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds(center);
@@ -18,10 +20,23 @@ export default function Map() {
     setMap(map);
   }, []);
 
+  const showLocationData = () => {
+    DataService.getData()
+      .then((response) => {
+        console.log("============Location is================");
+        console.log(response);
+        setLocation(response);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
   const onUnmount = useCallback(function callback(map) {
     setMap(null);
   }, []);
   console.log(map);
+  showLocationData();
   return (
     <div>
       <GoogleMap
---
+++ frontend/hackto2022/src/services/data.service.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = "https://annaostapenko.github.io/HackTo2022-BackEnd/data/fatal.json";
+
+const getData = async () => {
+  const response = await axios.get(API_URL);
+  return response.data;
+};
+
+
+export default {
+  getData
+};

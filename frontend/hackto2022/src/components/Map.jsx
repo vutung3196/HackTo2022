06c3a7c7f97956b4fca5f30c3e6541bca260cb54
yiktungyeung <yeungyiktung@CityDog-MacPro.local>
2022-09-10 15:29:09 -0400

    test places autocomplete

--- frontend/hackto2022/src/components/Map.jsx
+++ frontend/hackto2022/src/components/Map.jsx
@@ -1,12 +1,13 @@
 import { useState, useCallback } from "react";
 import { GoogleMap } from "@react-google-maps/api";
+import Places from "./Places";
 const center = {
   lat: -3.745,
   lng: -38.523,
 };
 const containerStyle = {
   width: "100vw",
-  height: "100hw",
+  height: "100vh",
 };
 
 export default function Map() {
@@ -21,9 +22,11 @@ export default function Map() {
   const onUnmount = useCallback(function callback(map) {
     setMap(null);
   }, []);
+
   console.log(map);
   return (
     <div>
+      <Places />
       <GoogleMap
         mapContainerStyle={containerStyle}
         zoom={12}

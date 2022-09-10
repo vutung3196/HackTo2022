6e896c5515362a6a21edf8febab188d481d016d3
sandyyim <48216946+sandyyim@users.noreply.github.com>
2022-09-10 17:14:55 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ app = Flask(__name__)
 
 def fetch_data(severity):
     df = pd.read_csv("./6719b900-1bf9-4678-b9a0-547e30a24846.csv", index_col ="_id", low_memory=False)
-    newdf = df.query('ACCLASS == "' + severity + '"')
-    neighborhood = newdf['NEIGHBOURHOOD'].value_counts()
+    querydf = df.query('ACCLASS == "' + severity + '"')
+    neighborhood = querydf['NEIGHBOURHOOD'].value_counts()
+    # outputdf = pd.DataFrame({'Neighbourhood': neighborhood.index, 'Count': neighborhood.values})
     return neighborhood
+
+def fetch_cordinates(severity):
+    df = pd.read_csv("./data_v2.csv", index_col ="_id", low_memory=False)
+    querydf = df.query('ACCLASS == "' + severity + '"')
+    outputdf = querydf[['NEIGHBOURHOOD', 'geometry']]
+    return outputdf
 
 @app.route("/")
 def hello():
@@ -18,13 +25,22 @@ def hello():
 @app.get("/fatal")
 def get_neighbourhood_fatal():
     data = fetch_data("Fatal").to_json()
-    print(data)
     return data,200
 
 @app.get("/nonfatal")
 def get_neighbourhood_nonfatal():
     data = fetch_data("Non-Fatal Injury").to_json()
-    print(data)
+    return data,200
+
+
+@app.get("/nonfatal_geo")
+def get_neighbourhood_nonfatal_geo():
+    data = fetch_cordinates("Non-Fatal Injury").to_json(orient='values')
+    return data,200
+
+@app.get("/fatal_geo")
+def get_neighbourhood_fatal_geo():
+    data = fetch_cordinates("Fatal").to_json(orient='values')
     return data,200
 
 if __name__ == "__main__":
